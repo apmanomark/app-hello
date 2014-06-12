e6c2741c3fdefd9d7137d231c50c1359d4280bcd
OpenShift guest <aptesttools@gmail.com>
2014-06-12 16:13:46 -0400

    added missing /unpack in sencha-cmd.sh paths

--- sencha-cmd.sh
+++ sencha-cmd.sh
@@ -1,12 +1,15 @@
 # script to intall Sencha SDK using Bash
 
+
 	mkdir ./unpack/
 
 	curl -o ./unpack/sencha-cmd.run.zip http://cdn.sencha.com/cmd/5.0.0.160/SenchaCmd-5.0.0.160-linux.run.zip
 	
-	unzip -p ./unpack/sencha-cmd.run.zip >./sencha-cmd.run
+	unzip -p ./unpack/sencha-cmd.run.zip >./unpack/sencha-cmd.run
 	rm ./unpack/sencha-cmd.run.zip
 
 	chmod +x ./unpack/sencha-cmd.run
 	./unpack/sencha-cmd.run --mode unattended --prefix ./sencha/
 	rm ./unpack/sencha-cmd.run
+	
+	rmdir ./unpack/
