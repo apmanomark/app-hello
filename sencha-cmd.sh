08dd00d26ad408bed546e10cafa4f2e83de523a8
OpenShift guest <aptesttools@gmail.com>
2014-06-12 16:05:00 -0400

    added / to unpack path

--- sencha-cmd.sh
+++ sencha-cmd.sh
@@ -1,9 +1,8 @@
 # script to intall Sencha SDK using Bash
 
-	mkdir ./unpack
+	mkdir ./unpack/
 
 	curl -o ./unpack/sencha-cmd.run.zip http://cdn.sencha.com/cmd/5.0.0.160/SenchaCmd-5.0.0.160-linux.run.zip
-
 	
 	unzip -p ./unpack/sencha-cmd.run.zip >./sencha-cmd.run
 	rm ./unpack/sencha-cmd.run.zip
