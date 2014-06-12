ce8e36efe2ef6b20530bbf6deaab05b2fa6f3874
OpenShift guest <aptesttools@gmail.com>
2014-06-12 18:34:53 -0400

    split sencha sh files

---
+++ sencha-download.sh
@@ -0,0 +1,5 @@
+
+	mkdir ./unpack/
+
+	curl -o ./unpack/sencha-cmd.run.zip http://cdn.sencha.com/cmd/5.0.0.160/SenchaCmd-5.0.0.160-linux.run.zip
+	
---
+++ sencha-install.sh
@@ -0,0 +1,10 @@
+	unzip -p ./unpack/sencha-cmd.run.zip >./unpack/sencha-cmd.run
+	rm ./unpack/sencha-cmd.run.zip
+
+	chmod +x ./unpack/sencha-cmd.run
+	./unpack/sencha-cmd.run --mode unattended --prefix ./sencha/
+	rm ./unpack/sencha-cmd.run
+	
+	rmdir ./unpack/
+	
+	source .bashrc
