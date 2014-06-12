536d2f6ff24c3c807f34de074024d2bb18b81e64
OpenShift guest <aptesttools@gmail.com>
2014-06-12 13:26:36 -0400

    added file sencha-cmd

---
+++ sencha-cmd.sh
@@ -0,0 +1,13 @@
+# script to intall Sencha SDK using Bash
+
+	mkdir bundle
+
+	curl -o bundle/sencha-cmd.run.zip http://cdn.sencha.com/cmd/5.0.0.160/SenchaCmd-5.0.0.160-linux.run.zip
+
+	
+	unzip -p bundle/sencha-cmd.run.zip > bundle/sencha-cmd.run
+	rm bundle/sencha-cmd.run.zip
+
+	chmod +x bundle/sencha-cmd.run
+	./bundle/sencha-cmd.run --mode unattended --prefix /sencha/
+	rm bundle/sencha-cmd.run
