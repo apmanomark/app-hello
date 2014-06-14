05c7d845761ec1e2af2362be7544eee434ac61e5
OpenShift guest <aptesttools@gmail.com>
2014-06-14 12:55:22 -0400

    added new sh

--- sencha-deft-install.sh
+++ sencha-deft-install.sh
@@ -1,3 +1,4 @@
+
 curl -o ./$SENCHA_DIR http://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/SenchaCmd$SENCHA_CMD_VERSION-linux-x64.run.zip
 unzip -q SenchaCmd$SENCHA_CMD_VERSION-linux-x64.run.zip
 chmod +x SenchaCmd$SENCHA_CMD_VERSION-linux-x64.run
