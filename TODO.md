0386f1fe8afca17fee4dcd040db487e84e74053c
OpenShift guest <aptesttools@gmail.com>
2014-06-14 12:47:13 -0400

    new sh install file

---
+++ TODO.md
@@ -0,0 +1,12 @@
+
+
+- Sencha unattended install
+
+    - unpack .run
+    ? set permissions
+    - check bash profile update
+    ? set env variables = DONE
+    
+    ? is it possible to run (not unpack) install file?
+    
+    ? add sencha.run file to repo = no need to download and unpack
--- sencha-install.sh
+++ sencha-install.sh
@@ -1,8 +1,48 @@
-	unzip -p ./unpack/sencha-cmd.run.zip >./unpack/sencha-cmd.run
-	rm ./unpack/sencha-cmd.run.zip
+    #Steps to setup sencha touch on linux
 
-	chmod +x ./unpack/sencha-cmd.run
-	./unpack/sencha-cmd.run --mode unattended --prefix ./sencha/
+        # Download Sencha Touch
+        
+            # done in download sh
+        
+        # unzip
+        
+
+
+        # Download SenchaCmd
+        
+            mkdir ./unpack/
+	        unzip -p ./unpack/sencha-cmd.run.zip >./unpack/sencha-cmd.run
+	        rm ./unpack/sencha-cmd.run.zip
+
+        # To install SenchaCmd, open a terminal, go to the location where SenchaCmd is downloaded and execute linux.run file
+            
+            chmod +x ./unpack/sencha-cmd.run
+	        ./unpack/sencha-cmd.run 
+
+        # Open the .bashrc file, and Add the following line at the end:
+            # 
+            
+            echo "export PATH=:/sencha/Sencha/Cmd/3.0.2.288" >> ~/.bash_profile
+            
+        # execute the command
+            # exec bash
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+    # --mode unattended --prefix ./sencha/
 	rm ./unpack/sencha-cmd.run
 	
 	rmdir ./unpack/
@@ -11,5 +51,18 @@
 	
 	cd ./sencha/Sencha
 	ls
-	export PATH=~/bin/Sencha/Cmd/5.0.0.160:$PATH
+	# export PATH=~/bin/Sencha/Cmd/5.0.0.160:$PATH
+	
+	
+	
+	# add line to ~/.bash_profile
+	
+	~/.bash_profile
+	
+	
+	
+	
+	
+	
+	
 	printenv
