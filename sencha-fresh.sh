01fff3812a64213c4728744bd57aa9722e8f22de
OpenShift guest <aptesttools@gmail.com>
2014-06-14 14:01:58 -0400

    tweak again

--- sencha-fresh.sh
+++ sencha-fresh.sh
@@ -1,11 +1,11 @@
 
 
 mkdir ./unpack/
-curl -o ./unpack/ http://cdn.sencha.com/cmd/${SENCHA_CMD_VERSION}/SenchaCmd${SENCHA_CMD_VERSION}-linux-x64.run.zip
+curl -o ./unpack/ http://cdn.sencha.com/cmd/${SENCHA_CMD_VERSION}/SenchaCmd-${SENCHA_CMD_VERSION}-linux-x64.run.zip
 
 mkdir ./${SENCHA_DIR}
-unzip -p ./unpack/${SENCHA_CMD_VERSION}/SenchaCmd${SENCHA_CMD_VERSION}-linux-x64.run.zip >./${SENCHA_DIR}/sencha-cmd.run
-rm ./unpack/${SENCHA_CMD_VERSION}/SenchaCmd${SENCHA_CMD_VERSION}-linux-x64.run.zip
+unzip -p ./unpack/${SENCHA_CMD_VERSION}/SenchaCmd-${SENCHA_CMD_VERSION}-linux-x64.run.zip >./${SENCHA_DIR}/sencha-cmd.run
+rm ./unpack/${SENCHA_CMD_VERSION}/SenchaCmd-${SENCHA_CMD_VERSION}-linux-x64.run.zip
 
 chmod +x ./${SENCHA_DIR}/sencha-cmd.run
 ./${SENCHA_DIR}/sencha-cmd.run --mode unattended 
