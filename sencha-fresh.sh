4423e39aa2b472ed001b42add79842cab22d31ef
OpenShift guest <aptesttools@gmail.com>
2014-06-14 13:10:35 -0400

    removed space in sh

--- sencha-fresh.sh
+++ sencha-fresh.sh
@@ -1,1 +1,1 @@
-curl -o ./$SENCHA_DIR http://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/SenchaCmd $SENCHA_CMD_VERSION-linux-x64.run.zip
+curl -o ./$SENCHA_DIR http://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/SenchaCmd$SENCHA_CMD_VERSION-linux-x64.run.zip
