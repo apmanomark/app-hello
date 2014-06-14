cb2e3827976f6a537d76e62dd5887db2f3d20279
OpenShift guest <aptesttools@gmail.com>
2014-06-14 13:18:20 -0400

    added sencha unzip and install to fresh sh

--- sencha-fresh.sh
+++ sencha-fresh.sh
@@ -1,1 +1,8 @@
 curl -o ./$SENCHA_DIR http://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/SenchaCmd$SENCHA_CMD_VERSION-linux-x64.run.zip
+
+unzip -q SenchaCmd${SENCHA_CMD_VERSION}-linux-x64.run.zip
+
+chmod +x SenchaCmd${SENCHA_CMD_VERSION}-linux-x64.run
+
+./SenchaCmd${SENCHA_CMD_VERSION}-linux-x64.run --mode unattended
+
