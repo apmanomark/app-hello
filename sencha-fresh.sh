80267f0e8c54d05e79cb58aedda06319df80b284
OpenShift guest <aptesttools@gmail.com>
2014-06-14 14:09:08 -0400

    tweaked yml

--- sencha-fresh.sh
+++ sencha-fresh.sh
@@ -2,14 +2,16 @@
 
 mkdir -p ./unpack/
 
-curl -o ./unpack/ http://cdn.sencha.com/cmd/${SENCHA_CMD_VERSION}/SenchaCmd-${SENCHA_CMD_VERSION}-linux-x64.run.zip
+curl -o ./unpack/sencha-cmd.run.zip http://cdn.sencha.com/cmd/5.0.0.160/SenchaCmd-5.0.0.160-linux.run.zip
 
 mkdir ./${SENCHA_DIR}/
-unzip -p ./unpack/SenchaCmd-${SENCHA_CMD_VERSION}-linux-x64.run.zip >./${SENCHA_DIR}/sencha-cmd.run
-rm ./unpack/SenchaCmd-${SENCHA_CMD_VERSION}-linux-x64.run.zip
 
-chmod +x ./${SENCHA_DIR}/sencha-cmd.run
-./${SENCHA_DIR}/sencha-cmd.run --mode unattended 
 
-export PATH=~/$SENCHA_DIR/Sencha/Cmd/$SENCHA_CMD_VERSION/:$SENCHA_DIR/node_modules/karma/bin:$SENCHA_DIR/node_modules/coffee-script/bin:$PATH
+unzip -p ./unpack/sencha-cmd.run.zip >./unpack/sencha-cmd.run
+rm -p ./unpack/sencha-cmd.run.zip
+
+chmod +x ./unpack/sencha-cmd.run
+./unpack/sencha-cmd.run --mode unattended 
+
+export PATH=~/unpack/Sencha/Cmd/$SENCHA_CMD_VERSION/:$SENCHA_DIR/node_modules/karma/bin:$SENCHA_DIR/node_modules/coffee-script/bin:$PATH
 
