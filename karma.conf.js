eb2cc1b3a19ef4d9e84fe5f72d170e360a4f0a6f
OpenShift guest <aptesttools@gmail.com>
2014-06-14 16:42:32 -0400

    added phatomjs to yml updated karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -3,9 +3,25 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
-
-    files: [
-      '*.js'
-    ]
+    
+    browsers: ['PhantomJS', 'PhantomJS_custom'],
+    
+    files: ['*.js'],
+    
+// you can define custom flags
+    customLaunchers: {
+      'PhantomJS_custom': {
+        base: 'PhantomJS',
+        options: {
+          windowName: 'my-window',
+          settings: {
+            webSecurityEnabled: false
+          }
+        },
+        flags: ['--remote-debugger-port=9000']
+      }
+    }    
+    
+    
   });
 };
