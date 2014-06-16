fc8cd2a471a28c1e3c9200756835619fbce79bb0
OpenShift guest <aptesttools@gmail.com>
2014-06-16 06:48:05 -0400

    added sh for uploading to testflight (ios only)

--- archive/sh/sencha-download.sh
+++
@@ -1,7 +0,0 @@
-
-	mkdir -p ./unpack/
-
-	curl -o ./unpack/sencha-cmd.run.zip http://cdn.sencha.com/cmd/5.0.0.160/SenchaCmd-5.0.0.160-linux.run.zip
-	
-	cd ./unpack/
-	ls
--- archive/sh/sencha-path.sh
+++
@@ -1,1 +0,0 @@
-export PATH=~/unpack/Sencha/Cmd/$SENCHA_CMD_VERSION/:$SENCHA_DIR/node_modules/karma/bin:$SENCHA_DIR/node_modules/coffee-script/bin:$PATH
