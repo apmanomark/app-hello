cfdca6894c1402ee480abd365c869510729dc012
ap_test_tools <aptesttools@gmail.com>
2014-06-18 16:13:51 +0000

    updated sencha build location path in phonegap build sh

--- source/scripts/bash/ci/phonegap_build.sh
+++ source/scripts/bash/ci/phonegap_build.sh
@@ -2,9 +2,23 @@
 
 # http://phonegap.com/blog/2013/11/20/SenchaPhoneGap/
 
-# sencha app build native 
+# sencha app build native
+
+pwd
+
+cd ./source/apps/qf-app-01
 
 sencha -d app build native > ci-buildlog.txt
 
 cat ci-buildlog.txt
+
+cd ../
+
+cd ../
+
+cd ../
+
+ls
+
+pwd
 
