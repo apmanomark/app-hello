459c94b3b5d32a79088f0ba56bd2bdba4b324e0f
ap_test_tools <aptesttools@gmail.com>
2014-06-18 14:58:58 +0000

    set build log and print log in phonegap build sh

--- source/scripts/bash/ci/phonegap_build.sh
+++ source/scripts/bash/ci/phonegap_build.sh
@@ -2,6 +2,9 @@
 
 # http://phonegap.com/blog/2013/11/20/SenchaPhoneGap/
 
-sencha app build -run native
+# sencha app build native 
 
+sencha -d app build native > ci-buildlog.txt
+
+cat ci-buildlog.txt
 
